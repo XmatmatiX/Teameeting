07f9cfdf907e47bfa7e579e3f2078d76253f4eb8
Mateusz Boroch <m.boroch2@gmail.com>
2022-02-15 21:05:11 +0100

    Add project files.

---
+++ Teameeting.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teameeting.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
