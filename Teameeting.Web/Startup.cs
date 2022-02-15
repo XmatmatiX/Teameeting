9b3ba05f41de30f0bc1193a2d7ed6bc47f5e8d23
Mateusz Boroch <m.boroch2@gmail.com>
2022-02-15 21:39:10 +0100

    add comments to dependency injection

--- Teameeting.Web/Startup.cs
+++ Teameeting.Web/Startup.cs
@@ -35,6 +35,9 @@ namespace Teameeting.Web
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            //Add dependen injection
+            //for example: services.AddTransient<InetrefaceName,ServiceName>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
